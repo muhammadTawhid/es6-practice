ae56e927439b01ce94d29d59fd2aa76c814be43c
muhammadtawhid <muhammadtawhid.me@gmail.com>
2021-08-23 21:42:38 +0600

    someUpdated

---
+++ 22-4templete.js
@@ -0,0 +1,19 @@
+const firstName = "ema";
+const lastName = "jhon";
+const fullName  = firstName +" " + lastName + " is a bussiness website";
+
+// using es6 templete
+const fullName2 = `${firstName} ${lastName} is a bussiness website`
+console.log(fullName2);
+
+const multipleLine = "i love u\n" 
++ "i also hate u\n"
++ "but i love u most.\n"
+
+// using es6
+const multipleLine2 = `i love u
+i hate u
+i don't want u
+bt remember i love u`
+
+console.log(multipleLine2);
